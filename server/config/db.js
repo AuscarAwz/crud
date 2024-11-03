07b0bd9cc37e220aede4359dbe69adbcdd3d5f84
AuscarAwz <askarsahib@gmail.com>
2024-11-03 14:45:21 +0530

    .env Update to gitfile

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,17 @@
-
 const mongoose = require('mongoose');
-//require('dotenv').config();
+require('dotenv').config();
 
 const connectDBRecords = async () => {
 
     try {
-        await mongoose.connect('mongodb+srv://askarsahib11:movie@movies.dqeyh.mongodb.net/crud?retryWrites=true&w=majority&appName=movies');
+        await mongoose.connect( process.env.MONGODB_URI );
         console.log("MongoDB connected. . .");
     } catch (error) {
         console.error(error.message);
         process.exit(1);
     }
 }
-
+//'mongodb+srv://askarsahib11:movie@movies.dqeyh.mongodb.net/crud?retryWrites=true&w=majority&appName=movies'
 
 
 module.exports = connectDBRecords;
