3103a1d9d6a03a35954ba2db8089186d079b7520
AuscarAwz <askarsahib@gmail.com>
2024-11-10 23:32:07 +0530

    Add Alternative ProductID based records  Delete API in controller

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDBRecords = async () => {
 
     try {
         await mongoose.connect( process.env.MONGODB_URI );
-        console.log("MongoDB connected. . .");
+        console.log("MongoDB connected . . .");
     } catch (error) {
         console.error(error.message);
         process.exit(1);
