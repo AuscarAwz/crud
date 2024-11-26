b275113025a03189e8e6d40ae220212a1d365769
AuscarAwz <askarsahib@gmail.com>
2024-11-27 01:29:53 +0530

    Add passwordbrcypt function in Controllers

--- controllers/empLoginControllers.js
+++ controllers/empLoginControllers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const empLoginModel = require('../model/loginModel.js');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 const routerControl = express.Router();
 
 exports.CreateUser = async (req, res) => {
@@ -8,7 +8,7 @@ exports.CreateUser = async (req, res) => {
     
     const { username, password } = req.body;
 
-    const passwordbcrypt = await bcrypt.hash(password, 10);
+    //const passwordbcrypt = await bcrypt.hash(password, 10);
 
     if(!username || !password) {
         return res.status(422).json({message: 'Username & password fields are required'})
@@ -38,7 +38,7 @@ exports.CreateUser = async (req, res) => {
     }*/
 
 
-    const userValue = new empLoginModel({ username, password, passwordbryt: passwordbcrypt }); //, passwordbryt: passwordbcrypt
+    const userValue = new empLoginModel({ username, password }); //, passwordbryt: passwordbcrypt
     
     console.log(userValue);
     
@@ -46,7 +46,7 @@ exports.CreateUser = async (req, res) => {
     
     //res.send(userValue);
 
-    return res.status(200).json({message: 'User created successfully',name:userDetails.username, password:userDetails.password, passwordbryt:userDetails.passwordbryt}) //, passwordbryt:userDetails.passwordbryt
+    return res.status(200).json({message: 'User created successfully',name:userDetails.username, password:userDetails.password}) //, passwordbryt:userDetails.passwordbryt
     
 } catch (error) {
         return res.status(500).json({message: error.message})
@@ -103,43 +103,43 @@ exports.GetUsersById = async (req, res) => {
 }
 }
 
-exports.UpdateLoginUserById = async (req, res) => {
-    try {
-        const { username, password, passwordbryt } = req.body;
-        const updatepasswordbcrypt = await bcrypt.hash(password, 10);
-        const userId = req.params._id;
-    //const { passwordbryt } = req.body
+// exports.UpdateLoginUserById = async (req, res) => {
+//     try {
+//         const { username, password, passwordbryt } = req.body;
+//         const updatepasswordbcrypt = await bcrypt.hash(password, 10);
+//         const userId = req.params._id;
+//     //const { passwordbryt } = req.body
 
-    //const updateEmp = await empLoginModel.findOneAndUpdate(empLoginModel => empLoginModel._id == req.params._id)
-    const updateEmp = await empLoginModel.findOne({ _id: userId })
+//     //const updateEmp = await empLoginModel.findOneAndUpdate(empLoginModel => empLoginModel._id == req.params._id)
+//     const updateEmp = await empLoginModel.findOne({ _id: userId })
     
-    console.log(updateEmp);
+//     console.log(updateEmp);
     
-    if(!updateEmp) {
-        return res.status(404).json({message: 'Product not found'})
-    }
+//     if(!updateEmp) {
+//         return res.status(404).json({message: 'Product not found'})
+//     }
 
-    //const {username, password, passwordbryt} = req.body
+//     //const {username, password, passwordbryt} = req.body
 
-    if(username) {
-        empLoginModel.username = req.body.username
-    }
-    if(password) {
-        empLoginModel.password = req.body.password
-    }
+//     if(username) {
+//         empLoginModel.username = req.body.username
+//     }
+//     if(password) {
+//         empLoginModel.password = req.body.password
+//     }
 
-    if(passwordbryt) {
-        empLoginModel.passwordbryt = updatepasswordbcrypt
-    }
+//     if(passwordbryt) {
+//         empLoginModel.passwordbryt = updatepasswordbcrypt
+//     }
     
-    // if('active' in req.body) {
-    //     product.active = active
-    // }
-    const updateEmpoyee = await updateEmp.save();
-    return res.status(200).json({message: 'Product updated successfully',name:updateEmpoyee.username, password:updateEmpoyee.password, passwordbryt:updateEmpoyee.passwordbryt})
+//     // if('active' in req.body) {
+//     //     product.active = active
+//     // }
+//     const updateEmpoyee = await updateEmp.save();
+//     return res.status(200).json({message: 'Product updated successfully',name:updateEmpoyee.username, password:updateEmpoyee.password, passwordbryt:updateEmpoyee.passwordbryt})
 
-} catch (error) {
-    return res.status(400).json({ message: error.message });
-}
+// } catch (error) {
+//     return res.status(400).json({ message: error.message });
+// }
 
-}
+// }
