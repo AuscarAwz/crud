4098560944b256d341ce72a6c22e59a620a7ef3c
AuscarAwz <askarsahib@gmail.com>
2024-11-27 01:25:48 +0530

    Add passwordbrcypt function in Controllers

--- controllers/empLoginControllers.js
+++ controllers/empLoginControllers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const empLoginModel = require('../model/loginModel.js');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const routerControl = express.Router();
 
 exports.CreateUser = async (req, res) => {
@@ -8,7 +8,7 @@ exports.CreateUser = async (req, res) => {
     
     const { username, password } = req.body;
 
-    //const passwordbcrypt = await bcrypt.hash(password, 10);
+    const passwordbcrypt = await bcrypt.hash(password, 10);
 
     if(!username || !password) {
         return res.status(422).json({message: 'Username & password fields are required'})
@@ -38,7 +38,7 @@ exports.CreateUser = async (req, res) => {
     }*/
 
 
-    const userValue = new empLoginModel({ username, password }); //, passwordbryt: passwordbcrypt
+    const userValue = new empLoginModel({ username, password, passwordbryt: passwordbcrypt }); //, passwordbryt: passwordbcrypt
     
     console.log(userValue);
     
@@ -46,7 +46,7 @@ exports.CreateUser = async (req, res) => {
     
     //res.send(userValue);
 
-    return res.status(200).json({message: 'User created successfully',name:userDetails.username, password:userDetails.password}) //, passwordbryt:userDetails.passwordbryt
+    return res.status(200).json({message: 'User created successfully',name:userDetails.username, password:userDetails.password, passwordbryt:userDetails.passwordbryt}) //, passwordbryt:userDetails.passwordbryt
     
 } catch (error) {
         return res.status(500).json({message: error.message})
@@ -86,4 +86,60 @@ exports.GetUserValidation = async (req, res) => {
     } catch (error) {
         return res.status(400).json({message: error.message})
     }
+}
+
+exports.GetUsersById = async (req, res) => {
+    try {
+        //const usersRecordsById = await empLoginModel.find( empLoginModel => empLoginModel._id ===  req.params._id );
+        const usersRecordsById = await empLoginModel.findById(req.params._id);;
+        console.log(usersRecordsById);
+
+        if(!usersRecordsById) {
+            return res.status(404).json({message: 'User not found'})
+        }
+        return res.status(200).json(usersRecordsById)
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+}
+}
+
+exports.UpdateLoginUserById = async (req, res) => {
+    try {
+        const { username, password, passwordbryt } = req.body;
+        const updatepasswordbcrypt = await bcrypt.hash(password, 10);
+        const userId = req.params._id;
+    //const { passwordbryt } = req.body
+
+    //const updateEmp = await empLoginModel.findOneAndUpdate(empLoginModel => empLoginModel._id == req.params._id)
+    const updateEmp = await empLoginModel.findOne({ _id: userId })
+    
+    console.log(updateEmp);
+    
+    if(!updateEmp) {
+        return res.status(404).json({message: 'Product not found'})
+    }
+
+    //const {username, password, passwordbryt} = req.body
+
+    if(username) {
+        empLoginModel.username = req.body.username
+    }
+    if(password) {
+        empLoginModel.password = req.body.password
+    }
+
+    if(passwordbryt) {
+        empLoginModel.passwordbryt = updatepasswordbcrypt
+    }
+    
+    // if('active' in req.body) {
+    //     product.active = active
+    // }
+    const updateEmpoyee = await updateEmp.save();
+    return res.status(200).json({message: 'Product updated successfully',name:updateEmpoyee.username, password:updateEmpoyee.password, passwordbryt:updateEmpoyee.passwordbryt})
+
+} catch (error) {
+    return res.status(400).json({ message: error.message });
+}
+
 }
--- routes/empRoute.js
+++ routes/empRoute.js
@@ -4,5 +4,7 @@ const empControllers = require('../controllers/empLoginControllers.js');
 router.post('/', empControllers.CreateUser);
 router.get('/', empControllers.GetAllUsers);
 router.get('/login', empControllers.GetUserValidation);
+router.get('/:_id', empControllers.GetUsersById);
+router.put('/:_id', empControllers.UpdateLoginUserById);
 
 module.exports = router
